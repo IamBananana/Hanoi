da632b9978b2376b2e21555f22dc90ce195bd131
Richard <110420851+IamBananana@users.noreply.github.com>
2024-09-20 11:17:38 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int antall = 3;
+        int antall = 9;
 
         flyttMange(antall, 'A', 'B', 'C');
         System.out.println(Math.round(Math.pow(2, antall)-1) + " forflytninger");
